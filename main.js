c58b26e5d4c5035d5eb5be83f451586c15e25d2d
Armand Gray <armandgray@gmail.com>
2016-12-19 16:16:19 -0800

    refactored code from addAnimationTo

--- main.js
+++ main.js
@@ -10,11 +10,24 @@ function whenWindowReady() {
 }
 
 function addAnimationTo(element, anim) {
-	if (element.className.indexOf('animated') < 0 && element.className.indexOf(anim) < 0) {
-		element.className += ' animated ' + anim;
-	} else if (element.className.indexOf('animated') < 0 && element.className.indexOf(anim) >= 0) {
-		element.className += ' animated';
-	} else if (element.className.indexOf('animated') >= 0 && element.className.indexOf(anim) < 0) {
-		element.className += ' ' + anim;
+	if (!isAnimated(element) && !hasAnimation(anim, element)) {
+		addClassTo(element, 'animated');
+		addClassTo(element, anim);
+	} else if (!isAnimated(element)) {
+		addClassTo(element, 'animated');
+	} else if (!hasAnimation(anim, element)) {
+		addClassTo(element, anim);
 	}
+}
+
+function isAnimated(element) {
+	return element.className.indexOf('animated') >= 0;
+}
+
+function hasAnimation(anim, element) {
+	return element.className.indexOf(anim) >= 0;
+}
+
+function addClassTo(element, className) {
+	element.className += ' ' + className;
 }
