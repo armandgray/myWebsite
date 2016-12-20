932e20cc07c9e1c81ade32da060ada3244c5c6b3
Armand Gray <armandgray@gmail.com>
2016-12-19 16:02:54 -0800

    added animation code for row[0]

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ window.onload = function() {
 };
 
 function whenWindowReady() {
-	$('#book').addClass('bounceOutLeft');
-	alert("working");
+	var rows = document.getElementsByClassName('row');
+	if (rows[0].className.indexOf('animated') < 0 && rows[0].className.indexOf('pulse') < 0) {
+		rows[0].className += ' animated pulse';
+	} else if (rows[0].className.indexOf('animated') < 0 && rows[0].className.indexOf('pulse') >= 0) {
+		rows[0].className += ' animated';
+	} else if (rows[0].className.indexOf('animated') >= 0 && rows[0].className.indexOf('pulse') < 0) {
+		rows[0].className += ' pulse';
+	}
+	// $('#row').addClass('animated pulse');
 }
