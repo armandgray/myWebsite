e2d0890dd060d2cd2f34b9f365e4958754cd4fd0
Armand Gray <armandgray@gmail.com>
2017-01-05 12:18:10 -0800

    added headerColorScrollListener in .js

--- item-detail/item-detail.js
+++ item-detail/item-detail.js
@@ -1,3 +1,16 @@
 $(window).load(function() {
-	alert('ready');
+	setupHeaderColorScrollListener();
 });
+
+function setupHeaderColorScrollListener() {
+	var backgroundColor = $('.container-fluid').css('background-color');
+	$('.header').css('background-color', 'rgba(0, 0, 0, 0)');
+
+	$(window).scroll(function() {
+	    if ($(this).scrollTop() > 200) {
+			$('.header').css('background-color', backgroundColor);
+	    } else {
+			$('.header').css('background-color', 'rgba(0, 0, 0, 0)');
+	    }
+	});
+}
