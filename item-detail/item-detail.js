2600756a40ea253a6acb5dce52f9cfc0c9a40f0e
Armand Gray <armandgray@gmail.com>
2017-01-05 18:17:02 -0800

    prepares slider for adding animation

--- item-detail/item-detail.js
+++ item-detail/item-detail.js
@@ -1,8 +1,10 @@
 var backgroundColor = $('.container-fluid').css('background-color');
 
 $(window).load(function() {
-	setupHeaderColorScrollListener();
 	$('.gallery-images').css('background-color', backgroundColor);
+
+	setupHeaderColorScrollListener();
+	setupSliderAnim();
 });
 
 function setupHeaderColorScrollListener() {
@@ -13,6 +15,16 @@ function setupHeaderColorScrollListener() {
 			$('.header').css('background-color', backgroundColor);
 	    } else {
 			$('.header').css('background-color', 'rgba(0, 0, 0, 0)');
+	    }
+	});
+}
+
+function setupSliderAnim() {
+	$(window).scroll(function() {
+	    if ($(this).scrollTop() > 20) {
+			$('.slider').hide();
+	    } else {
+			$('.slider').show();
 	    }
 	});
 }
