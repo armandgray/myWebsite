19002aeb35aee61343cddd22baede2fff0e508c4
Armand Gray <armandgray@gmail.com>
2017-01-17 13:40:47 -0800

    added setupItemsNavListener() method to resources.js with fields for each sublist of items

--- resources.js
+++ resources.js
@@ -1,38 +1,28 @@
-window.onload = function() {
-	whenWindowReady();
-};
+$(document).ready(function() {
+	setupItemsNavListener();
+});
 
-function whenWindowReady() {
-	addListenersToIcons();
-}
+function setupItemsNavListener() {
+    var itemSelectors = $('.items-nav button');
+    var items = $('.item-container');
+    var favorites = $('.favorites');
+    var assets = $('.assets');
+    var projects = $('.projects');
+    var android = $('.android');
 
-function addListenersToIcons() {
-	var iconsList = document.getElementsByClassName('icon');
-	for (var i = 0; i < iconsList.length; i++) {
-		var img = iconsList[i].childNodes[1];
-		img.addEventListener('mouseover', addAnimationTo(img, 'bounce'), false);
+    alert(itemSelectors[4].innerHTML);
+
+    $(itemSelectors[0]).click(function() {
+    	for (var i = 0; i < items.length; i++) {
+    		$(items[i]).show();
+    	}
+    });
+
+    $(itemSelectors[1]).click(function() {
+    	for (var i = 0; i < items.length; i++) {
+    		if (items.indexOf(items[i]) == -1 {
+
+    		}
+    	}
 	}
-}
-
-function addAnimationTo(element, anim) {
-	if (!isAnimated(element) && !hasAnimation(anim, element)) {
-		addClassTo(element, 'animated');
-		addClassTo(element, anim);
-	} else if (!isAnimated(element)) {
-		addClassTo(element, 'animated');
-	} else if (!hasAnimation(anim, element)) {
-		addClassTo(element, anim);
-	}
-}
-
-function isAnimated(element) {
-	return element.className.indexOf('animated') >= 0;
-}
-
-function hasAnimation(anim, element) {
-	return element.className.indexOf(anim) >= 0;
-}
-
-function addClassTo(element, className) {
-	element.className += ' ' + className;
 }
