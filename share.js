11da0833ccdfe4ef0183e929a9530110c2e7f492
Armand Gray <armandgray@gmail.com>
2017-01-17 19:46:31 -0800

    Added snackbar message on send button click

--- share.js
+++ share.js
@@ -6,6 +6,11 @@ $("document").ready(function() {
 
 function setupSendMessageListener() {
 	$('.btn-send').click(function() {
-		alert('message sent');
+		// Get the snackbar DIV
+	    var x = document.getElementById("snackbar")
+	    // Add the "show" class to DIV
+	    x.className = "show";
+	    // After 3 seconds, remove the show class from DIV
+	    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 	});
 }
