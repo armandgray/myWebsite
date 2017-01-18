1847629d196ee0c6cd17de69c4656a9631c81297
Armand Gray <armandgray@gmail.com>
2017-01-17 16:01:13 -0800

    added listeners for each selectors and animDuration

--- resources.js
+++ resources.js
@@ -1,17 +1,19 @@
 var items;
 
+var animDuration = 500;
+
 $(document).ready(function() {
 	setupItemsNavListener();
 });
 
 function setupItemsNavListener() {
     items = $('.item-container');
-    var itemSelectors = $('.items-nav button');
-
     var favorites = $('.favorites');
     var assets = $('.assets');
     var projects = $('.projects');
-    var android = $('.android');
+    var gettingStartedItems = $('.getting-started');
+
+    var itemSelectors = $('.items-nav button');
 
     $(itemSelectors[0]).click(function() {
     	showAllItems();
@@ -20,19 +22,31 @@ function setupItemsNavListener() {
     $(itemSelectors[1]).click(function() {
     	onlyShow(favorites);
     });
+
+    $(itemSelectors[2]).click(function() {
+    	onlyShow(assets);
+    });
+
+    $(itemSelectors[3]).click(function() {
+    	onlyShow(projects);
+    });
+
+    $(itemSelectors[4]).click(function() {
+    	onlyShow(gettingStartedItems);
+    });
 }
 
 function showAllItems() {
 	for (var i = 0; i < items.length; i++) {
-		$(items[i]).show();
+		$(items[i]).show(animDuration);
 	}
 }
 
 function onlyShow(itemsList) {
 	for (var i = 0; i < items.length; i++) {
-		$(items[i]).hide();
+		$(items[i]).hide(animDuration);
 	}
 	for (var i = 0; i < itemsList.length; i++) {
-		$(itemsList[i]).show();
+		$(itemsList[i]).show(animDuration);
 	}
 }
