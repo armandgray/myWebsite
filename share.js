1d91c6953dce4b55a366f5593220141aad1bc472
Armand Gray <armandgray@gmail.com>
2017-01-17 19:33:52 -0800

    added click listener to send button

--- share.js
+++ share.js
@@ -1,4 +1,11 @@
 $("document").ready(function() {
 	changeFooterDisplay(true);
+	setupSendMessageListener();
 });
 
+
+function setupSendMessageListener() {
+	$('.btn-send').click(function() {
+		alert('message sent');
+	});
+}
