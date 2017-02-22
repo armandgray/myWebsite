d6d6276f0fdf277450f723fbb6e1966a756230c0
Armand Gray <armandgray@gmail.com>
2017-02-22 14:35:33 -0800

    moved code from finish webpack into current project (working)

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: "dist/webpack",
+		filename: "bundle.js",
+		publicPath: "webpack"
+	},
+	devServer: {
+		inline: true,
+		contentBase: './dist',
+		port: 3000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: ["babel-loader"],
+				query: {
+					presets: ["latest", "stage-0", "react"]
+				}
+			}
+		]
+	}
+}
