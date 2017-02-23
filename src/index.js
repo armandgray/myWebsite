817328b1c9e2737fb07e406c4a1063f6704b3085
Armand Gray <armandgray@gmail.com>
2017-02-23 09:02:59 -0800

    small styling fixes for footer-nav

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 
 const style = {
+	marginTop: 20,
 	marginLeft: '5%',
 	width:'90%',
 	paddingLeft:25,
@@ -17,56 +18,38 @@ const footerColor = {
 }
 
 render(
-	<div id="bottom-footer"
-		className="page-footer-copyright" 
-		style={ style }>
+	<div>
+		<div id="footer-menu" style={ style }></div>
+		<div id="footer-nav" className="page-footer-copyright" style={ style }></div>
+	</div>,
+	document.getElementById('react-container')
+)
+
+render(
+	<div>
 		<p style={{ cssFloat:"left" }}>
 			Copyright © 2016 Armand Gray - Digital Portfolio
 		</p>
 		<p style={{ width:'78%' }}>
 			<a href="landing.html">
-			<img id="footer-personal-logo" 
-				src="assets/personal_logo_949494.svg" 
-				width="15" 
-				height="11" 
-				alt="personal logo" 
-				style={{ 
-					marginTop:-2.5, 
-					marginRight:7 
-				}}></img>
+				<img id="footer-personal-logo" src="assets/personal_logo_949494.svg" width="15" height="11" alt="personal logo" style={{ marginTop:-2.5, marginRight:7 }}></img>
 			</a>
-			<a href="resources.html" 
-				id="explore-btn" 
-				style={ footerColor }>
+			<a href="resources.html" id="explore-btn" style={ footerColor }>
 				Explore
 			</a>
 			&nbsp; | &nbsp; 
-			<a href="share.html"
-				style={ footerColor }>
+			<a href="share.html" style={ footerColor }>
 				Share
 			</a>
 			&nbsp; | &nbsp; 
-			<a href="#bottom-footer"
-				id="more-less-btn"
-				style={ footerColor }>
+			<a href="#bottom-footer" id="more-less-btn" style={ footerColor }>
 				Less
 			</a>
 		</p>
-		<p id="language" 
-			style={{
-				cssFloat:"right", 
-				marginTop:-25
-			}}>
-			<img 
-				style={{ 
-					marginTop:-2.5, 
-					marginRight:7 
-				}} 
-				src="assets/icons/ic_circular_us_flag.png" 
-				height="15" 
-				width="15">
-			</img>United States
+		<p id="language" style={{ cssFloat:"right", marginTop:-25 }}>
+			<img style={{ marginTop:-2.5, marginRight:7 }} src="assets/icons/ic_circular_us_flag.png" height="15" width="15"></img>
+			United States
 		</p>
 	</div>,
-	document.getElementById('react-container')
+	document.getElementById('footer-nav')
 )
